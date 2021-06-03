4920e8333d7b251128207d6c50cb6213378b2127
TechnoBoy <Dloadjunior@gmail.com>
2021-06-03 06:01:51 +0530

    Students API Controller

---
+++ BasicAuthentication/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BasicAuthentication.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            using(StudentsDBEntities student = new StudentsDBEntities())
+            {
+                return Ok(student.Students.ToList());
+            }
+        }
+    }
+}
